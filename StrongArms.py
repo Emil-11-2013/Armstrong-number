5b45d8a1bc33b58584a9a02fa29e4bdb177faf85
Emil-11-2013 <emilgeorgek@gmail.com>
2025-02-23 15:31:38 +0300

    Add files via upload

---
+++ StrongArms.py
@@ -0,0 +1,25 @@
+# Take input from the User
+number = int(input("Input your number: "))
+
+
+# Calculate number of digits
+digits = len(str(number))
+
+
+#Initialize result variable
+resultNumber = 0
+
+
+# Find the sum of the a^digits of each digit
+temp = number
+while temp > 0:
+    digit = temp % 10
+    resultNumber += digit ** digits
+    temp //= 10
+
+
+    # Display the result
+if number == resultNumber:
+        print(number,"is an Armstrong number")
+else:
+        print(number,"is not an Armstrong number")
